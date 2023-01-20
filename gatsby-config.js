2acd5e991d607fb73a4ac8c0e73f91abeecf5cd4
Daniel Johansson <daniel.johansson@kdiko.se>
2023-01-20 15:08:50 +0100

    Optimize font loading using Fontsource
    Replaced gatsby-omni-font-loader with self-hosted
    fonts using Fontsource since loading of fonts was
    slow causing FOUT.

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,21 +18,5 @@ module.exports = {
         icon: "src/images/kdiko-logo.png",
       },
     },
-    {
-      resolve: "gatsby-omni-font-loader",
-      options: {
-        enableListener: true,
-        preconnect: [
-          "https://fonts.googleapis.com",
-          "https://fonts.gstatic.com",
-        ],
-        web: [
-          {
-            name: "Montserrat",
-            file: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap",
-          },
-        ],
-      },
-    },
   ],
 };
