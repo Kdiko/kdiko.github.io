efbad8114aa70bbaf96b70d7f6d212fad416478e
Daniel Johansson <daniel.johansson@kdiko.se>
2023-01-20 16:31:58 +0100

    Add Verdana as fallback font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import "@fontsource/montserrat";
-import "@fontsource/montserrat/600.css";
 import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/400.css";
 import "./src/styles/global.css";
